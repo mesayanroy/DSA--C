f2b0556956714d92b253352742d24dd9942f215d
Sayan Roy <169074736+mesayanroy@users.noreply.github.com>
2025-12-10 04:29:42 +0530

    Implement Shell Sort algorithm in shellsort.c

---
+++ Sorting/shellsort.c
@@ -0,0 +1,13 @@
+void shellSort(int a[], int n){
+    for(int gap=n/2; gap>0; gap/=2){
+        for(int i=gap; i<n; i++){
+            int temp=a[i];
+            int j=i;
+            while(j>=gap && a[j-gap] > temp){
+                a[j] = a[j-gap];
+                j -= gap;
+            }
+            a[j] = temp;
+        }
+    }
+}
